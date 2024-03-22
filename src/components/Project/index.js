72a2868007e8667284bcec2472e940d0fb3949e6
KituIco <kimtuico_06@rocketmail.com>
2024-03-22 18:51:44 +0800

    added video demo

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import ReactPlayer from 'react-player/youtube'
 import './index.scss';
 
 import homeworksLogo from '../../assets/media/logos/homeworks.png';
@@ -9,34 +10,50 @@ import gsptsLogo from '../../assets/media/logos/gspts.png';
 import gsptsBanner from '../../assets/media/banner/gspts.png';
 import cincoLogo from '../../assets/media/logos/cinco.png';
 import cincoBanner from '../../assets/media/banner/cinco.png';
+import playLogo from '../../assets/media/play.png';
 
 const Project = ({ navHeader }) => {
   const [resume, setResume] = useState(false);
   const [proj, setProj] = useState('HomeWorks');
+
+  const [video, setVideo] = useState(false);
+  const [url, setUrl] = useState('https://youtu.be/kMi8tj6d6b0');
   const projs = ['HomeWorks', 'NotesOn', 'GSPTS', 'Cinco!'];
   const info = [
     { name: 'HomeWorks: Home Services App', logo: homeworksLogo, banner: homeworksBanner,
       stack: ['Express.JS', 'Node.JS', 'React Native', 'Expo', 'Figma', 'MySQL', 'Docker', 'Socket.IO', 'Android Studio', 'Google Cloud'],
-      description: 'Providing a convenient, reliable, and hassle-free way of finding and booking house related services, HomeWorks Mobile App is a general platform that connects users with various home and wellness service providers in their area.'
+      description: 'Providing a convenient, reliable, and hassle-free way of finding and booking house related services, HomeWorks Mobile App is a general platform that connects users with various home and wellness service providers in their area.',
+      url:'https://youtu.be/kMi8tj6d6b0'
     },
     { name: 'NotesOn WebApp: A Notion Clone', logo: notesonLogo, banner: notesonBanner,
       stack: ['Express.JS', 'Node.JS', 'Angular', 'Bootstrap', 'CSS', 'MySQL', 'Redis', 'Docker', 'Socket.IO', 'Typescript'],
-      description: 'Seamlessly organize notes, assignments, and collaborative efforts, NotesOn WebApp is a Notion Clone which is a versatile productivity tool that combines note taking, project management, and database functionalities into one platform.'
+      description: 'Seamlessly organize notes, assignments, and collaborative efforts, NotesOn WebApp is a Notion Clone which is a versatile productivity tool that combines note taking, project management, and database functionalities into one platform.',
+      url:'https://youtu.be/Kfo9i51PTfI'
     },
     { name: 'Graduate Student Progress Tracking System', logo: gsptsLogo, banner: gsptsBanner,
       stack: ['Python', 'Django', 'SQLite','HTML', 'Tailwind CSS', 'Mockplus'],
-      description: 'For a simpler, more user-friendly, and less error-prone tracking system, DCS GSPTS is a web application for managing proposed plan of study particularly for graduate students under the Department of Computer Science in UP Diliman.'
+      description: 'For a simpler, more user-friendly, and less error-prone tracking system, DCS GSPTS is a web application for managing proposed plan of study particularly for graduate students under the Department of Computer Science in UP Diliman.',
+      url:'https://youtu.be/OD9fpvjc4as'
     },
     { name: 'Uno, Dos, Tres... Cinco!', logo: cincoLogo, banner: cincoBanner,
       stack: ['Python', 'Pyglet'],
-      description: 'Recreating a typical UP student’s drive for excellence, Uno Dos Tres, CINCO! is a drop-and-catch game that places the player in the life of an Isko trying to obtain passing marks while avoiding the failing grade that comes in the way.'
+      description: 'Recreating a typical UP student’s drive for excellence, Uno Dos Tres, CINCO! is a drop-and-catch game that places the player in the life of an Isko trying to obtain passing marks while avoiding the failing grade that comes in the way.',
+      url:'https://youtu.be/QC-8aVHVsic'
     },
   ]
 
+  const onChangeCard = ( id, url ) => {
+    setProj(id);
+    setUrl(url);
+  }
+
   const format = (comp) => {
     return (
-      <div className={`card ${proj===projs[comp] ? 'highlight' : ''}`} onClick={() => setProj(projs[comp])}>
+      <div className={`card ${proj===projs[comp] ? 'highlight' : ''}`} onClick={() => onChangeCard(projs[comp],info[comp].url) }>
         <img className={`banner ${proj===projs[comp] ? 'highlight' : ''}`} src={info[comp].banner} alt={info[comp].name}/>
+        <div className={`player ${proj===projs[comp] ? 'highlight' : ''}`} onClick={() => setVideo(true)}>
+          <img className='playcon' src={playLogo} alt="play logo"/>
+        </div>
         <div className={`title ${proj===projs[comp] ? 'highlight' : ''}`}>
           {info[comp].name}
         </div>
@@ -60,11 +77,20 @@ const Project = ({ navHeader }) => {
     <>
       <div className='projects' id='projects' ref={navHeader[4].headerRef}>
         <div className='content'>
-          <h1>projects</h1>   
+          <h1>projects</h1>
+          <h4 className='details'>Explore the culmination of my projects! Hover over and click on each project to unveil a quick demo video.</h4>
+
           <div className='cardsContainer'>
             { projects }
           </div>
         </div>
+      </div>
+
+      <div className={`videos ${video ? '' : 'hide'}`}>
+        <div className='area' onClick={() => setVideo(false)}/>
+        <div className='player'> {
+          video && <ReactPlayer  url={url} playing={true} loop={true} controls={true} width="80vw" height="45vw" top="20"/> 
+        } </div>
       </div>
 
       <div className='resume'>
